6be7cd0ae495210904de2cdae2f25698030f0d24
Majid Fouladpour <majid@binary.com>
2015-11-12 05:55:19 +0330

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -51914,7 +51914,7 @@ pjax_config_page('rise_fall_table', function() {
     };
 });
 
-pjax_config_page('portfolio|trade.cgi|statement|f_manager_statement|f_manager_history|' +
+pjax_config_page('portfoliows|portfolio|trade.cgi|statement|f_manager_statement|f_manager_history|' +
     'f_profit_table|profit_table|trading|statementws|profit_tablews', function() {
     return {
         onLoad: function() {
@@ -58025,6 +58025,187 @@ $(function() {
       active: false
     });
 });
+;var PortfolioWS =  (function() {
+
+    'use strict';
+
+    var pageLanguage = page.language();
+    if(!pageLanguage) pageLanguage = "EN";
+
+    var rowTemplate;
+    var retry;
+
+    var init = function() {
+        // get the row template and then discard the node as it has served its purpose
+        rowTemplate = $("#portfolio-dynamic tr:first")[0].outerHTML;
+        $("#portfolio-dynamic tr:first").remove();
+        TradeSocket.init({"then_do":"updateBalance"});
+    };
+
+    
+    var updateBalance = function(data) {
+        $("span[data-id='balance']").text(fixCurrency(data.authorize.balance, data.authorize.currency));
+        if(parseFloat(data.authorize.balance, 10) > 0) {
+            $("#if-balance-zero").remove();
+        }
+    };
+
+    /**
+     * Updates portfolio table
+    **/
+    var updatePortfolio = function(data) {
+
+        /**
+         * Check for error
+        **/
+        if("error" in data) {
+            throw new Error("Trying to get portfolio data, we got this error", data.error);
+        }
+
+        /**
+         * no contracts
+        **/
+        if(0 === data.portfolio.contracts.length) {
+            $("#portfolio-table").remove();
+            $("#portfolio-no-contract").removeClass("dynamic");
+            return true;
+        }
+
+        /**
+         * User has at least one contract
+        **/
+
+        $("#portfolio-no-contract").remove();
+        var contracts = '';
+        var sumPurchase = 0.0;
+        var currency;
+        $.each(data.portfolio.contracts, function(ci, c) {
+            sumPurchase += parseFloat(c.buy_price, 10);
+            currency = c.currency;
+            contracts += rowTemplate
+            .split("!transaction_id!").join(c.transaction_id)
+            .split("!contract_id!").join(c.contract_id)
+            .split("!longcode!").join(c.longcode)
+            .split("!currency!").join(c.currency)
+            .split("!buy_price!").join(fixCurrency(c.buy_price));
+        });
+
+        // contracts is ready to be added to the dom
+        $("#portfolio-dynamic").replaceWith(trans(contracts));
+
+        // update footer area data
+        sumPurchase = sumPurchase.toFixed(2);
+        $("#cost-of-open-positions").text( fixCurrency(sumPurchase, currency));
+
+        // request "proposal_open_contract"
+        TradeSocket.send({"proposal_open_contract":1});
+
+        // ready to show portfolio table
+        $("#trading_init_progress").remove();
+        $("#portfolio-content").removeClass("dynamic");
+
+    };
+
+    var updateIndicative = function(data) {
+
+        if("error" in data) {
+            // if api returns a response like the one below. 
+            // we assume the indicative to be 0 if this happens
+            /*
+            {
+              "echo_req": {
+                "proposal_open_contract": 1
+              },
+              "error": {
+                "code": "ContractSellValidationError",
+                "message": "Resale of this contract is not offered."
+              },
+              "proposal_open_contract": {
+                "id": "c4f8b5176928a7823ff09f8f44a51528"
+              },
+              "msg_type": "proposal_open_contract"
+            }
+            */
+            return false;
+        }
+
+        $("tr[data-contract_id='"+data.proposal_open_contract.contract_id+"'] strong.indicative_price").text(data.proposal_open_contract.ask_price);
+
+        var indicative_sum = 0, indicative_price = 0;
+        $("strong.indicative_price").each(function() {
+            indicative_price = $(this).text();
+            indicative_price = parseFloat(indicative_price, 2);
+            if(!isNaN(indicative_price)) {
+                indicative_sum += indicative_price;
+            }
+        });
+
+        indicative_sum = indicative_sum.toFixed(2);
+
+        $("#value-of-open-positions").text(fixCurrency(indicative_sum, "USD"));
+
+    };
+
+
+    /*** utility functions ***/
+
+    // Dynamic text
+    var dTexts = ["view", "indicative"];
+
+    /**
+     * In the dynamic parts we have strings to include
+     * For instance, in portfolio table, we have a 'View' button
+     * for each contract.
+    **/
+    var trans = function(str) {
+        var placeholder;
+        for(var i = 0, l = dTexts.length; i < l; i++) {
+            placeholder = ":"+dTexts[i]+":";
+            if(-1 === str.indexOf(placeholder)) continue;
+            str = str.split(placeholder).join(text.localize(dTexts[i]));
+        }
+        return str;
+    };
+
+    /**
+     * Amounts received from the API could be integer or decimal numbers.
+     * In case we have an integer, like 73, we want to display a decimal
+     * with two fractions, i.e. 73:00
+     * This function does that.
+     * Adapted from http://stackoverflow.com/a/14428340
+     * Kudos to: [VisioN](http://stackoverflow.com/users/1249581)
+    **/
+    var fixCurrency = function(n, c) {
+        var currency = ""; 
+        if("number" !== typeof n) n = parseFloat(n);
+        var snum = n + "", dec;
+        if(-1 === snum.indexOf(".")) {
+            dec = 2;
+        } else {
+            dec = snum.split(".")[1].length;
+        }
+        if("string" === typeof c) {
+            currency = c + " ";
+        }
+        return currency + " " + n.toFixed(dec).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+    };
+ 
+    return {
+        init: init,
+        updateBalance: updateBalance,
+        updatePortfolio: updatePortfolio,
+        updateIndicative: updateIndicative
+    };
+
+})();
+
+pjax_config_page('portfoliows', function() {
+    return {
+        onLoad: function() {
+            PortfolioWS.init();
+        }
+    };
+});
 ;function currencyConvertorCalculator()
 {
     var currencyto = document.getElementById('currencyto');
@@ -60687,41 +60868,120 @@ var TradingEvents = (function () {
  * events based on type of response
  */
 var Message = (function () {
-    'use strict';
+    "use strict";
 
     var process = function (msg) {
-        var response = JSON.parse(msg.data);
-        if (response) {
-            var type = response.msg_type;
-            if (type === 'authorize') {
-                TUser.set(response.authorize);
-                TradeSocket.send({ payout_currencies: 1 });
-            } else if (type === 'active_symbols') {
-                processActiveSymbols(response);
-            } else if (type === 'contracts_for') {
-                processContract(response);
-            } else if (type === 'payout_currencies') {
-                sessionStorage.setItem('currencies', msg.data);
-                displayCurrencies();
-            } else if (type === 'proposal') {
-                processProposal(response);
-            } else if (type === 'buy') {
-                Purchase.display(response);
-            } else if (type === 'tick') {
-                processTick(response);
-            } else if (type === 'trading_times'){
-                processTradingTimes(response);
-            } else if (type === 'statement'){
-                StatementWS.statementHandler(response);
-            } else if (type === 'profit_table'){
-                ProfitTableWS.profitTableHandler(response);
-            } else if (type === 'error') {
-                $(".error-msg").text(response.error.message);
-            }
-        } else {
 
-            console.log('some error occured');
+        var response;
+
+        try {
+            response  = JSON.parse(msg.data);
+            if("object" !== typeof response || !("msg_type" in response)) {
+                throw new Error("Response from WS API is not well formatted.");
+            }
+        } catch(e) {
+            throw new Error("Response from WS API is not well formatted.");
         }
+
+        var msg_type = response.msg_type;
+        
+        switch(msg_type) {
+
+            case "error":
+                $(".error-msg").text(response.error.message);
+                break;
+
+            case "authorize":
+
+                var then_do = "unknown";
+                if("passthrough" in response.echo_req && "then_do" in response.echo_req.passthrough) {
+                    then_do = response.echo_req.passthrough.then_do;
+                }
+
+                switch(then_do) {
+                    case "updateBalance":
+                        PortfolioWS.updateBalance(response);
+                        TradeSocket.send({"portfolio": 1});
+                        break;
+
+                    default:
+                        TUser.set(response.authorize);
+                        TradeSocket.send({ payout_currencies: 1 });
+                }
+                break;
+
+            case "active_symbols":
+                processActiveSymbols(response);
+                break;
+
+            case "contracts_for":
+                processContract(response);
+                break;
+
+            case "payout_currencies":
+                sessionStorage.setItem("currencies", msg.data);
+                displayCurrencies();
+                break;
+
+            case "proposal":
+                processProposal(response);
+                break;
+
+            case "buy":
+                Purchase.display(response);
+                break;
+
+            case "tick":
+                processTick(response);
+                break;
+
+            case "trading_times":
+                processTradingTimes(response);
+                break;
+
+            case "statement":
+                StatementWS.statementHandler(response);
+                break;
+
+            case "balance":
+                var passthroughObj = response.echo_req.passthrough;
+                if (passthroughObj){
+                    switch (passthroughObj.purpose) {
+                        case "statement_footer":
+                            StatementUI.updateStatementFooterBalance(response.balance);
+                            break;
+                        default :
+                            //do nothing
+                    }
+                }
+                break;
+
+            case 'profit_table':
+                ProfitTableWS.profitTableHandler(response);
+                break;
+
+            case "portfolio":
+                PortfolioWS.updatePortfolio(response);
+                break;
+
+            case "proposal_open_contract":
+                PortfolioWS.updateIndicative(response);
+                break;
+
+            case "change_password":
+                PasswordWS.apiResponse(response);
+                break;
+
+            case "forget":
+            case "forget_all":
+                // no action needed
+                break;
+
+            default:
+                throw new Error("No method exits to handle api message of type '" + msg_type + "'.");
+
+        }
+
     };
 
     return {
@@ -61485,17 +61745,26 @@ var TradeSocket = (function () {
         }
     };
 
-    var init = function () {
+    var init = function (passthrough) {
+
         if(!isClose()){
             return;
         }
-
+        
         tradeSocket = new WebSocket(socketUrl);
 
         tradeSocket.onopen = function (){
             var loginToken = getCookieItem('login');
             if(loginToken) {
-                tradeSocket.send(JSON.stringify({authorize: loginToken}));
+                if("object" === typeof passthrough) {
+                    var j = {
+                        authorize: loginToken,
+                        passthrough: passthrough
+                    };
+                    tradeSocket.send(JSON.stringify(j));
+                } else {
+                    tradeSocket.send(JSON.stringify({authorize: loginToken}));
+                }
             } else {
                 tradeSocket.send(JSON.stringify({ payout_currencies: 1 }));
             }
